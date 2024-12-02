1166caa3a75cbad1c5559c124305bce6c316a33a
Murad <67361462+Murad04@users.noreply.github.com>
2024-12-02 23:44:06 +0900

    add improts

--- cloud/backend/main.py
+++ cloud/backend/main.py
@@ -10,7 +10,7 @@ import logging
 from pushbullet import API
 from cloud.backend.base_logger import log_function
 from quart_cors import cors
-from ml_yolov5 import model_yolo_for_recog as M_Yolo_Recog
+from cloud.ml_yolov5 import model_yolo_for_recog as M_Yolo_Recog
 from socketio import SocketIO
 import asyncio,datetime
 
